07f032e0a7d0c4020bebc10ed34105a448356245
espisepi <sepinaco@gmail.com>
2021-05-20 17:54:35 +0200

    delete cita functionality added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ function App() {
     ]);
   }
 
+  // Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id );
+    // guardarCitas(nuevasCitas);  Tambien funciona
+    guardarCitas([
+      ...nuevasCitas
+    ])
+  }
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -34,6 +43,7 @@ function App() {
               <Cita
                 key={cita.id}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))}
           </div>
